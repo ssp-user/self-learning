62955f1a29ad76e89463ac6c27d2c449f3e961e1
Vishal Joshi <joshivishal@yahoo.com>
2020-08-01 21:00:34 +0530

    Updated files

--- src/main/java/com/app/screens/LoginPage.java
+++ src/main/java/com/app/screens/LoginPage.java
@@ -1,6 +1,13 @@
 package com.app.screens;
 
-public class LoginPage {
-	
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class LoginPage {
+	WebDriver driver = new FirefoxDriver(); 
+// - https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin
+	
+	public void login(){	
+		//driver.findElement(By.id("account")).click();		
+	}
 }
