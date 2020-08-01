3db0de837c05d840f06b267da87949e2677de52f
Vishal Joshi <joshivishal@yahoo.com>
2020-08-01 22:56:39 +0530

    New changes

--- src/main/java/com/app/screens/LoginPage.java
+++ src/main/java/com/app/screens/LoginPage.java
@@ -1,5 +1,7 @@
 package com.app.screens;
 
+import java.util.Base64;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,12 +14,15 @@ public class LoginPage {
 		System.setProperty("webdriver.chrome.driver","C:\\Setup\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin");
+		driver.manage().window().maximize();
 		
 	}
 	
-	public void login(){	
+	public void login(){
+		String decrptData  ="b3JhY2xlMjVleWU=";
+		byte[] decodeBytes = Base64.getDecoder().decode(decrptData.getBytes());
 		driver.findElement(By.id("username")).sendKeys("joshivishal@yahoo.com");		
-		driver.findElement(By.id("password")).sendKeys("oracle25eye");
+		driver.findElement(By.id("password")).sendKeys(new String(decodeBytes));
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 	}
 }
