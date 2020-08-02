c85f31950488156cdf660077d12384aa6e30c026
Vishal Joshi <joshivishal@yahoo.com>
2020-08-02 18:12:32 +0530

    Implemented Extent reports

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,42 @@
+package pages;
+ 
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+ 
+public class BasePage {
+    public WebDriver driver;
+    public WebDriverWait wait;
+ 
+    //Constructor
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+ 
+    //Click Method
+    public void click(By elementLocation) {
+        waitVisibility(elementLocation);
+        driver.findElement(elementLocation).click();
+    }
+ 
+    //Write Text
+    public void writeText(By elementLocation, String text) {
+        waitVisibility(elementLocation);
+        driver.findElement(elementLocation).sendKeys(text);
+    }
+ 
+    //Read Text
+    public String readText(By elementLocation) {
+        waitVisibility(elementLocation);
+        return driver.findElement(elementLocation).getText();
+    }
+ 
+    //Wait
+    public void waitVisibility(By by){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+}
---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,33 @@
+package pages;
+ 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+ 
+public class HomePage extends BasePage {
+ 
+    /**Constructor*/
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+ 
+    /**Variables*/
+    String baseURL = "http://www.n11.com/";
+ 
+    /**Web Elements*/
+    By signInButtonClass = By.className("btnSignIn");
+    By modal = By.xpath("//*[@id=\"userKvkkModal\"]/div/div[2]/span");
+ 
+    /**Page Methods*/
+    public HomePage goToN11() {
+        driver.get(baseURL);
+        if(driver.findElement(modal).isDisplayed()) {
+            click(modal);
+        }
+        return this;
+    }
+ 
+    public LoginPage goToLoginPage() {
+        click(signInButtonClass);
+        return new LoginPage(driver);
+    }
+}
