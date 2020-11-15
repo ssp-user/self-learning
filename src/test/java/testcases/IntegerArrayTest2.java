bb36ced722a4ef3a5c6c41e0db1186cbd50f0ffd
Vishal Joshi <Vishal_Joshi@dealer-fx.com>
2020-11-15 13:47:26 +0530

    New file

---
+++ src/test/java/testcases/IntegerArrayTest2.java
@@ -0,0 +1,25 @@
+package testcases;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class IntegerArrayTest2 {
+
+	public static void main(String[] args) {
+		Integer array1[] = {10,20,13,30,27,15,36};
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println(array1[i]);
+		}		
+		Integer array2[] = {30,40,12,27,20,227};
+		ArrayList al1 = new ArrayList();
+		Arrays.sort(array1);
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println("****" +array1[i]);
+		}	
+		System.out.println("Trying Arrays binary search " +Arrays.binarySearch(array1, 15));		
+		System.out.println("Trying Arrays binary search " +Arrays.binarySearch(array1, 9000));
+
+	}
+}
