e0066c9f568554236e3b82e736954f2618b3eaf3
Vishal Joshi <Vishal_Joshi@dealer-fx.com>
2020-11-29 12:42:09 +0530

    update

--- src/test/java/puzzles_medium/Sub_array_A.java
+++ src/test/java/puzzles_medium/Sub_array_A.java
@@ -1,20 +1,42 @@
 package puzzles_medium;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class Sub_array_A {
 
 	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("1", "2");
-		map.put("21", "3");
-		map.put("1", "5");
-		map.put("21", "21");		
-		for (Map.Entry<String,String> entry : map.entrySet())  
-            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		Integer array[] = {4,2,-3,-1,0,4};
+		int desiredSum = 3;
+
+		int length = array.length;
+		/*
+		 * int i,j = 0; while (true) { incrementalSum = incrementalSum + array[i]; i++;
+		 * }
+		 */
+		
+		for (int i = 0; i < array.length; i++) {
+			int incrementalSum = array[i];			
+			Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+			map.put(i, array[i]);			
+			for (int j = i + 1; j < array.length; j++) {
+				System.out.println("Value of i is "+ i + " Value of j is "+ j);
+				map.put(j, array[j]);
+				//System.out.println("Pre increment sum is "+ incrementalSum);
+				incrementalSum = incrementalSum + array[j];
+				System.out.println("Post increment sum is "+ incrementalSum);
+				if (incrementalSum == desiredSum) {
+					System.out.println("***************Sub Array Found*******");
+					for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+						System.out.print(entry.getValue() + "   ");
+					}
+					System.out.println("");
+					System.out.println("***********************");
+				}
+//				map.put(j, array[j]);				
+	//			incrementalSum = incrementalSum + array[j];				
+			}
+		}
 		
 	}
 }
