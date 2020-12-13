e83dd3421abc79713cf2aa4731c7d60e411a383e
Vishal Joshi <Vishal_Joshi@dealer-fx.com>
2020-12-13 16:27:10 +0530

    New case

---
+++ src/test/java/testcases/TestException.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class TestException {
+
+	public static void main(String[] args) {
+		File file = new File("");
+		try {
+			FileInputStream stream = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+
+}
