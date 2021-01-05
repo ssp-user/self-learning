4a6538d973cc06bb1500ac3a79079126b0c036dc
Vishal Joshi <Vishal_Joshi@dealer-fx.com>
2021-01-05 15:02:01 +0530

    learning

--- src/main/java/arrayListConcept/ArrayListMethods.java
+++ src/main/java/arrayListConcept/ArrayListMethods.java
@@ -1,6 +1,8 @@
 package arrayListConcept;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class ArrayListMethods {
 
@@ -12,6 +14,7 @@ public class ArrayListMethods {
 		ar1.add("javascript");
 		ArrayList<String> ar2 = new ArrayList<String>();
 		ar2.add("testing");
+		ar2.add("java");		
 		ar2.add("devops");
 		//ar1.addAll(ar2);
 		ar1.addAll(2, ar2);
@@ -22,8 +25,28 @@ public class ArrayListMethods {
 		System.out.println(ar1);
 		ar1.stream().forEach(ele->System.out.println(ele));
 		cloneList.stream().forEach(ele->System.out.println(ele));
-		
-		
+		System.out.println(cloneList.contains("devops"));
+		System.out.println(cloneList.containsAll(ar1));
+		System.out.println(cloneList.indexOf("devops"));
+		System.out.println(cloneList.lastIndexOf("java"));
+		ArrayList<Integer> number = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		number.stream().forEach(ele->System.out.println(ele));
+		number.removeIf(vj -> vj%2 != 0);
+		System.out.println("***********");
+		number.stream().forEach(ele -> System.out.println(ele));
+		ArrayList<String> namesList = new ArrayList<String>(Arrays.asList("Vishal","Joshi","Samanyu","Joshi"));
+		System.out.println(namesList);
+		namesList.retainAll(Collections.singleton("Joshi"));
+		System.out.println(namesList);
+		ArrayList<String> namesList2 = new ArrayList<String>(Arrays.asList("Vishal","Joshi","Samanyu","Vanad","Prasad"));
+		System.out.println(namesList2);
+		Object[] obj =   namesList2.toArray();
+		for (Object string : obj) {
+			System.out.println(string.toString());
+		}
+		namesList2.stream().forEach(ele -> System.out.println(ele));
+		namesList2.subList(1, 4).stream().forEach(ele->System.out.println(ele));
 	}
 
 }
+
