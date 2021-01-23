91876f574b93ed359f7535df945697a6ecc9da90
Vishal Joshi <Vishal_Joshi@dealer-fx.com>
2021-01-23 08:00:05 +0530

    add

---
+++ src/test/java/puzzles_medium/EvenOdd.java
@@ -0,0 +1,39 @@
+package puzzles_medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Integer inputArray[] = {12, 34, 45, 9, 8, 90, 3};
+		List<Integer> inputArrayList = Arrays.asList(inputArray);
+//		Set<Integer> oddSet = new HashSet<Integer>();
+		Set<Integer> oddSet = new LinkedHashSet<Integer>();		
+//		Set<Integer> evenSet = new HashSet<Integer>();
+		Set<Integer> evenSet = new LinkedHashSet<Integer>();		
+		for (int i = 0; i < inputArray.length; i++) {
+			if (inputArray[i]%2 == 0) {
+				evenSet.add(inputArray[i]);
+				//System.out.println(inputArray[i]+ " number is even");
+			}
+			else {
+				oddSet.add(inputArray[i]);
+				//System.out.println(inputArray[i]+ " number is odd");				
+			}
+		}
+		ArrayList<Integer> outputArrayList = new ArrayList<Integer>(evenSet);
+		outputArrayList.addAll(oddSet);
+		System.out.println("input array is "+ inputArrayList);		
+		System.out.println("output array is "+ outputArrayList);
+		for (Integer integer : outputArrayList) {
+			System.out.println(integer);
+		}
+	}
+
+}
