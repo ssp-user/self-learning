612aa2d50eb135bf7327f17592fb390a32254e57
vishaljoshi <vishaljoshi@ip-192-168-29-24.ap-south-1.compute.internal>
2022-01-07 22:18:00 +0530

    Started New Test

---
+++ src/test/java/puzzles_medium/TestEvenOdd.java
@@ -0,0 +1,12 @@
+package puzzles_medium;
+// Source - https://www.geeksforgeeks.org/segregate-even-and-odd-numbers/?ref=lbp
+public class TestEvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input[] = {12, 34, 45, 9, 8, 90, 3};
+		//Expected output = Output = {12, 34, 8, 90, 45, 9, 3}
+
+	}
+
+}
