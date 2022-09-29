9c19a0773feea0a5a60036d160a17deabddc1998
vishaljoshi <vishaljoshi@ip-192-168-29-24.ap-south-1.compute.internal>
2022-09-29 20:01:06 +0530

    New Test case

---
+++ src/test/java/learning/VJTest.java
@@ -0,0 +1,15 @@
+package learning;
+
+public class VJTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "My Name is Vishal Joshi";
+		String [] arr = str.split(" ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("String at index "+ i + " is " + arr[i]);
+		}
+
+	}
+
+}
